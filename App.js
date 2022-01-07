02fe079a148134ad369026a58f89e20057c30ae2
vinithkumar096 <vinithkumar42@gmail.com>
2022-01-07 14:12:28 -0500

    navigation added

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import AccountScreen from "./screens/AccountScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Account : AccountScreen,
+    Login: LoginScreen,
+    SignUp: SignUpScreen,
     
   },
   {
