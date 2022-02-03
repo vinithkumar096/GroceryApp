3f1f37919eaf5532b53308521d17380b36c705dd
udayReddy <udaydheerajreddy@gmail.com>
2022-02-03 11:17:14 -0500

    Update AdminOrderItem.js
    AdminOrderItem wrong changes before

--- Components/AdminOrderItem.js
+++ Components/AdminOrderItem.js
@@ -1,55 +1,84 @@
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-elements";
-import Icon from "react-native-vector-icons/FontAwesome5";
-import AdminOrderItem from "./AdminOrderItem";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-const ViewOrdersScreen = ({navigation}) => {
-    const goBack = () => {
-        navigation.navigate("AccountScreen");
-      }
+const AdminOrderItem = () => {
     return (
-        <ScrollView>
         <View style={styles.container}>
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View style={styles.orderBy}>
+                <View>
+                    <Text style={{ fontSize: 12, fontWeight: "bold" }}>Order by</Text>
+                    <Text>udaydheeraj</Text>
+                </View>
+                <Text style={styles.itemsCount}>2 items</Text>
+            </View>
+            <View style={styles.dateView}>
+                <Text style={{ fontSize: 12, fontWeight: "bold" }}>Date</Text>
+                <Text>tuesday jan 11</Text>
+            </View>
+            <View style={styles.dateView}>
+                <Text style={{ fontSize: 12, fontWeight: "bold" }}>Status</Text>
+                <Text style={{ color: "#008000" }}>placed</Text>
+            </View>
+            <View style={styles.btnsView}>
                 <Button
-                    buttonStyle={styles.btnGoBack}
+                    icon={<Icon name="check-all" size={24} style={styles.iconStyle} />}
+                    iconPosition="right"
+                    title="Delivered"
+                    buttonStyle={styles.btnStyle}
+                    
+                />
+                <Button
                     icon={
                         <Icon
-                            name="chevron-left"
-                            color={"#FFFFFF"}
-                            size={14}
-                            style={{ textAlign: "center" }}
+                            name="account-check-outline"
+                            size={24}
+                            style={[styles.iconStyle, { color: "#000" }]}
                         />
-                        
                     }
-                    onPress={goBack}
-
+                    iconPosition="right"
+                    title="Pick up"
+                    titleStyle={{ color: "#000" }}
+                    buttonStyle={[styles.btnStyle, { backgroundColor: "#DED7CD" }]}
+                    
                 />
-                <Text style={{ fontWeight: "bold", fontSize: 22, marginLeft: 32 }}>
-                    Admin Orders Page
-                </Text>
             </View>
-            <ScrollView>
-            <Text style={{textAlign: "center", color: "gray", marginTop: 32}}>No orders</Text>
-            <AdminOrderItem />
-            </ScrollView>
         </View>
-        </ScrollView>
     )
 }
-export default  ViewOrdersScreen;
+export default AdminOrderItem;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        padding: 20,
-        paddingTop:40,
-    },
-    btnGoBack: {
-        width: 30,
-        height: 30,
-        backgroundColor: "#EF7215",
-        color: "#FFFFFF",
+        marginTop: 20,
+        padding: 4,
+        borderWidth: 1,
         borderRadius: 8,
+        borderColor: "#000",
+    },
+    orderBy: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "flex-end",
+    },
+    itemsCount: {
+        fontSize: 12,
+        fontWeight: "bold",
+    },
+    dateView: {
+        marginTop: 12,
+    },
+    btnsView: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 12,
+    },
+    btnStyle: {
+        borderRadius: 8,
+        backgroundColor: "#EF7215",
+    },
+    iconStyle: {
+        marginLeft: 12,
+        color: "#FFFFFF",
     },
 });
